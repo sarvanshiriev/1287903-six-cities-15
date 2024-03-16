c985af577f66b4affcc2b43973d13756059e7f5e
Sarvan Shiriev <sarvan.shiriev@mail.ru>
2024-03-16 17:23:02 +0500

    Обратная сторона реальности

---
+++ src/components/place-card-list/place-card-list.tsx
@@ -0,0 +1,22 @@
+import PlaceCard from '../place-card/place-card';
+import { Offers } from '../../types/offers';
+
+type PlaceCardListProps = {
+    offerList: Offers;
+    setCardHoverId(id: string | null): void;
+}
+
+function PlaceCardList({ offerList, setCardHoverId }: PlaceCardListProps): JSX.Element {
+  return (
+    <div className="cities__places-list places__list tabs__content">
+      {offerList.map((offer) => {
+        const keyValue = offer.id;
+        return (
+          <PlaceCard key={keyValue} offerCard={offer} setCardHoverId={setCardHoverId} />
+        );
+      })}
+    </div>
+  );
+}
+
+export default PlaceCardList;
---
+++ src/types/offers.ts
@@ -0,0 +1,34 @@
+type Offer = {
+  id: string;
+  title: string;
+  type: string;
+  price: number;
+  city: {
+    name: string;
+      location: {
+      latitude: number;
+      longitude: number;
+      zoom: number;
+    };
+  };
+  location: {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+  };
+  isFavorite: boolean;
+  isPremium: boolean;
+  rating: number;
+  description: string;
+  bedrooms: number;
+  goods: string[];
+  host: {
+  name: string;
+  avatarUrl: string;
+  isPro: boolean;
+  };
+  images: string[];
+  maxAdults: number;
+  }
+
+export type Offers = Offer[];
