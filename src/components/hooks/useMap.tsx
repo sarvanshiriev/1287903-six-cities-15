710afa59b8c1b516ac066af76ab018610ab22388
Sarvan Shiriev <sarvan.shiriev@mail.ru>
2024-03-26 16:43:00 +0500

    Больше подробностей (часть 1)

---
+++ src/components/hooks/useMap.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState, useRef } from 'react';
+import leaflet from 'leaflet';
+import { Map } from 'leaflet';
+import { CityMap } from '../../types/city-map';
+
+function useMap(mapRef: React.MutableRefObject<HTMLElement | null>, city: CityMap): Map | null {
+  const [map, setMap] = useState<Map | null>(null);
+  const isRenderedRef = useRef<boolean>(false);
+
+  useEffect(() => {
+    if (mapRef.current !== null && !isRenderedRef.current) {
+      const instance = leaflet.map(mapRef.current, {
+        center: {
+          lat: city.lat,
+          lng: city.lng,
+        },
+        zoom: city.zoom,
+      });
+
+      leaflet
+        .tileLayer(
+          'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+          {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+          },
+        )
+        .addTo(instance);
+
+      setMap(instance);
+      isRenderedRef.current = true;
+    }
+  }, [mapRef, city]);
+  return map;
+}
+
+export default useMap;
---
+++ src/mocks/city.ts
@@ -0,0 +1,10 @@
+import {CityMap} from '../types/city-map';
+
+const city: CityMap = {
+  title: 'Amsterdam',
+  lat: 52.3909553943508,
+  lng: 4.85309666406198,
+  zoom: 10,
+};
+
+export {city};
